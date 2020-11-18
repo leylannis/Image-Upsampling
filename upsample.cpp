240d8ff294229f2abef2dc6f05c6ad3a36476a77
Leylanni Quijano-Shafer <lsq17@my.fsu.edu>
2020-11-18 14:48:19 +0000

    created a resize function that moves provided grayscale values to a new matrix with double the size, filling gaps with "-1" to mark they are unknown for later interpolation purposes

--- upsample.cpp
+++ upsample.cpp
@@ -10,6 +10,7 @@ using namespace std;
 // function declarations
 vector< vector<int> > parse_input(char* filename);
 void print_matrix(char* filename, vector< vector<int> > matrix);
+vector< vector<int> > resize_matrix(vector< vector<int> > matrix);
 vector< vector<int> > nearest_neighbor(vector< vector<int> > matrix);
 vector< vector<int> > my_interpolation(vector< vector<int> > matrix);
 
@@ -21,8 +22,9 @@ int main(int argc, char* argv[]) {
     vector< vector<int> > newmatrix;
 
     matrix = parse_input(inputfile);    
+    newmatrix = resize_matrix(matrix);
 
-    newmatrix = nearest_neighbor(matrix);
+    newmatrix = nearest_neighbor(newmatrix);
     //newmatrix = my_interpolation(matrix);
 
     print_matrix(outputfile, newmatrix);
@@ -47,6 +49,19 @@ vector< vector<int> > my_interpolation(vector< vector<int> > matrix){
 }
 
 vector<vector<int> > nearest_neighbor(vector< vector<int> > matrix){
+
+    for (int i=0; i<matrix.size()*matrix.size(); i++){
+        if ( (i/matrix.size())%2 == 0){
+	    if (matrix[i/matrix.size()][i%matrix.size()] == -1)
+		matrix[i/matrix.size()][i%matrix.size()] = matrix[i/matrix.size()][i%matrix.size() - 1];
+	}
+	else{
+	    matrix[i/matrix.size()][i%matrix.size()] = matrix[i/matrix.size() -1][i%matrix.size()];
+	}
+    }
+    return matrix;
+
+/*
     vector< vector<int> > newmatrix;
     for (int i=0; i<matrix.size()*2; i++) {
         vector<int> newrow;
@@ -54,6 +69,30 @@ vector<vector<int> > nearest_neighbor(vector< vector<int> > matrix){
 	    newrow.push_back(matrix[i/2][j/2]);
         }                
 	newmatrix.push_back(newrow);
+    }
+    return newmatrix;
+*/
+}
+
+vector< vector<int> > resize_matrix(vector< vector<int> > matrix){
+    vector< vector<int> > newmatrix;
+
+    for (int i=0; i<matrix.size()*2; i++) {
+        vector<int> newrow;
+        for (int j=0; j<matrix.size()*2; j++) {
+	    if (i%2 == 0){
+	        if (j%2 == 0){
+		    newrow.push_back(matrix[i/2][j/2]);
+		}
+		else{
+		    newrow.push_back(-1);	// empty/invalid pixel marker
+		}
+	    }
+	    else{
+                newrow.push_back(-1);		// empty/invalid pixel marker
+	    }
+        }
+        newmatrix.push_back(newrow);
     }
     return newmatrix;
 }
