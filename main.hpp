d0e0ddd9dd08f8967b95c295764d187ae79dd659
Leylanni Quijano-Shafer <lsq17@my.fsu.edu>
2020-11-19 04:46:25 +0000

    updated main.hpp

--- main.hpp
+++ main.hpp
@@ -13,3 +13,26 @@ vector< vector<int> > resize_matrix(vector< vector<int> > matrix);
 vector< vector<int> > nearest_neighbor(vector< vector<int> > matrix);
 vector< vector<int> > my_interpolation(vector< vector<int> > matrix);
 
+vector< vector<int> > resize_matrix(vector< vector<int> > matrix){
+    vector< vector<int> > newmatrix;
+
+    for (int i=0; i<matrix.size()*2; i++) {
+        vector<int> newrow;
+        for (int j=0; j<matrix.size()*2; j++) {
+            if (i%2 == 0){
+                if (j%2 == 0){
+                    newrow.push_back(matrix[i/2][j/2]);
+                }
+                else{
+                    newrow.push_back(-1);       // empty/invalid pixel marker
+                }
+            }
+            else{
+                newrow.push_back(-1);           // empty/invalid pixel marker
+            }
+        }
+        newmatrix.push_back(newrow);
+    }
+    return newmatrix;
+}
+
