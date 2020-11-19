cd9c39c17bff3d37898c86dcae4fbd846ea34b76
Leylanni Quijano-Shafer <lsq17@my.fsu.edu>
2020-11-19 15:56:18 +0000

    created more unit tests, still need to test some components, and make existing tests more thorough

--- UnitTest.cpp
+++ UnitTest.cpp
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
 #include "main.hpp"
 
-TEST(UnitTest, FillNeg1Gaps){
+TEST(UnitTest, ResizeNeg1Fill){
+    // resize_matrix() maps original image matrix to double the size
+    // a 4x4 matrix will be resized to an 8x8 with -1 in all new spaces
+
     std::vector<vector<int>> matrix04 (4, vector<int>(4,0));
     std::vector<vector<int>> matrix08 (8, vector<int>(8,0));
 
@@ -21,15 +24,28 @@ TEST(UnitTest, PopulateSmallMatrix){
 }
 
 TEST(UnitTest, TestNearestNeighbor){
+    std::vector<vector<int>> matrix1;
+    std::vector<int> row0 = {2,-1,3,-1};
+    std::vector<int> row13 = {-1,-1,-1,-1};
+    std::vector<int> row2 = {4,-1,5,-1};
+    matrix1.push_back(row0);
+    matrix1.push_back(row13);
+    matrix1.push_back(row2);
+    matrix1.push_back(row13);
+
+    ASSERT_EQ(2, nearest_neighbor(matrix1)[1][1]);
+    ASSERT_EQ(2, nearest_neighbor(matrix1)[0][1]);
+
+// add more statements here !!
 
 }
 
 TEST(UnitTest, TestMyInterpolation){
-
+	// unit test for my_interpolation()
 }
 
 TEST(UnitTest, PrintingMatricies){
-
+	// test for correct printing of matricies
 }
 
 int main(int argc, char **argv) {
