c87968b987ab0763d5bff5b93259a48e8a1174ee
Leylanni Quijano-Shafer <lsq17@my.fsu.edu>
2020-11-22 04:55:29 +0000

    Update README.md, main.cpp, main.hpp files

--- main.hpp
+++ main.hpp
@@ -13,42 +13,6 @@ vector< vector<int> > resize_matrix(vector< vector<int> > matrix);
 vector< vector<int> > nearest_neighbor(vector< vector<int> > matrix);
 vector< vector<int> > my_interpolation(vector< vector<int> > matrix);
 
-void print_matrix(char* filename, vector< vector<int> > matrix){
-    ofstream outfile(filename);
-
-    if (outfile.is_open()){
-        for (int i=0; i < matrix.size(); i++){
-            for (int j=0; j < matrix[i].size(); j++){
-                outfile << matrix[i][j] << ",";
-            }
-            outfile << endl;
-        }
-    }
-}
-
-vector< vector<int> > resize_matrix(vector< vector<int> > matrix){
-    vector< vector<int> > newmatrix;
-
-    for (int i=0; i<matrix.size()*2; i++) {
-        vector<int> newrow;
-        for (int j=0; j<matrix.size()*2; j++) {
-            if (i%2 == 0){
-                if (j%2 == 0){
-                    newrow.push_back(matrix[i/2][j/2]);
-                }
-                else{
-                    newrow.push_back(-1);       // empty/invalid pixel marker
-                }
-            }
-            else{
-                newrow.push_back(-1);           // empty/invalid pixel marker
-            }
-        }
-        newmatrix.push_back(newrow);
-    }
-    return newmatrix;
-}
-
 vector< vector<int> > my_interpolation(vector< vector<int> > matrix){
     for (int i=0; i< matrix.size()*matrix.size(); i++){
         int row = i/matrix.size();
@@ -111,3 +75,38 @@ vector<vector<int> > parse_input(char* filename){
     }
 }
 
+void print_matrix(char* filename, vector< vector<int> > matrix){
+    ofstream outfile(filename);
+
+    if (outfile.is_open()){
+        for (int i=0; i < matrix.size(); i++){
+            for (int j=0; j < matrix[i].size(); j++){
+                outfile << matrix[i][j] << ",";
+            }
+            outfile << endl;
+        }
+    }
+}
+
+vector< vector<int> > resize_matrix(vector< vector<int> > matrix){
+    vector< vector<int> > newmatrix;
+
+    for (int i=0; i<matrix.size()*2; i++) {
+        vector<int> newrow;
+        for (int j=0; j<matrix.size()*2; j++) {
+            if (i%2 == 0){
+                if (j%2 == 0){
+                    newrow.push_back(matrix[i/2][j/2]);
+                }
+                else{
+                    newrow.push_back(-1);       // empty/invalid pixel marker
+                }
+            }
+            else{
+                newrow.push_back(-1);           // empty/invalid pixel marker
+            }
+        }
+        newmatrix.push_back(newrow);
+    }
+    return newmatrix;
+}
